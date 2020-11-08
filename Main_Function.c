ecbeeeebd9235c7bed0d0d9527cf270145890f88
Yuri Domingos <64822018+YuriDomingos@users.noreply.github.com>
2020-11-08 08:52:41 +0100

    Update Main_Function.c

--- Main_Function.c
+++ Main_Function.c
@@ -1,7 +1,5 @@
 /*
 @Autor : Yuri Domingos
-Orientador : Msc.Eng : Solander Agostinho 
-Disciplina : Sistemas Inteligentes 1 
 Data   : 19 -10 -2020
 
 Objectivo : Implementar as funcoes necessárias para realizar o percurso exaustivo de um grafo
